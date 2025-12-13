872d290035e8021672bb145d6db01f16fddeefda
InnovativeSumit <pal2024santosh@gmail.com>
2025-12-13 16:35:35 +0530

    DAY 5

--- CHAPTER 5/SETS/set.py
+++ CHAPTER 5/SETS/set.py
@@ -1,23 +1,42 @@
-e =set() # do not ise s={} because it creates an empty dictionary
-print(e)
+a=()
+print(a)
+print(type(a))
 
-# create a tuples 
-sum = (10,20,30,10,20)
-print(sum)
-print(type(sum))
+a1=("sumit",45,True,89.98,{"key1":"value1","key2":"value2"},(1,2,3),[4,5,6])
+print(a1)
+print(type(a1))
 
-#create a set
-sum ={10,20,30,10,20}
-print(sum)
-print(type(sum))
+b ={}
+print(b)
+print(type(b))
 
+a2={
+    "name":"sumit",
+    "age":45,
+    "is_student":True,
+    "marks":89.98,
+    "details":{"key1":"value1","key2":"value2"},
+    "tuple_data":(1,2,3),
+    "list_data":[4,5,6]
+}
+print(a2)
+print(type(a2))
 
-#create a list 
-sum =[10,20,30,10,20]
-print(sum)
-print(type(sum))
+c=[]
+print(c)
+print(type(c))
 
+c1=["sumit",45,True,89.98,{"key1":"value1","key2":"value2"},(1,2,3),[4,5,6],{7,0,9}]
+print(c1)
+print(c1[7])
+print(type(c1))
 
-data_set = {"Sumit", 20, True, 3.5}
-print(data_set)
-print(type(data_set))
+d=set()
+print(d)
+print(type(d))
+
+d1={"sumit",45,True,89.98,(1,2,3)}
+# a1=("sumit",45,True,89.98,{"key1":"value1","key2":"value2"},(1,2,3),[4,5,6]) we cannot insert list and dict inside of a set because list is mutable
+print(d1)
+print(type(d1))
+
